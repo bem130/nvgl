8295b9f40713e12215d40506b8020565088dab8d
bem130 <haruki070617@gmail.com>
2024-02-24 11:08:00 +0900

    change a lot

--- nvgl.js
+++ nvgl.js
@@ -64,7 +64,6 @@ var NVGLang = (function () {
     };
     NVGLang.prototype.parseTree_TLnodes = function (nodes_span, indent) {
         this.parseTree = { type: "root", span: [0, this.code.length], include: null, init: null, timeline: null, obj: [], func: [] };
-        var nodes = [];
         var i = nodes_span[0];
         var indentstat = false;
         var lineinfo = [];
@@ -103,7 +102,7 @@ var NVGLang = (function () {
             }
             i++;
         }
-        lineinfo.push(["eos", i + 1]);
+        lineinfo.push(["eos", i]);
         console.log(lineinfo);
         var j = 0;
         while (j < lineinfo.length) {
@@ -126,7 +125,7 @@ var NVGLang = (function () {
                     while (lineinfo[k][0] == "indents") {
                         k++;
                     }
-                    this.parseTree.init = this.parseTree_node([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1, "init", "line");
+                    this.parseTree.init = this.parseTree_func_body([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1);
                     j = k - 1;
                 }
                 else {
@@ -146,9 +145,37 @@ var NVGLang = (function () {
                     throw "error ".concat(this.getLineAndCol(i).toString());
                 }
             }
+            if (lineinfo[j][0] == "obj") {
+                var k = j + 1;
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    this.parseTree.obj.push(this.parseTree_obj([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1, "timeline", "line"));
+                    j = k - 1;
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString());
+                }
+            }
+            if (lineinfo[j][0] == "func") {
+                var k = j + 1;
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    var func = { type: "func", span: [lineinfo[j][1] + indent * this.indentSpace, lineinfo[k][1] - 1], child: null };
+                    func.child = this.parseTree_func_body([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1);
+                    this.parseTree.func.push(func);
+                    j = k - 1;
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString());
+                }
+            }
             j++;
         }
-        return nodes;
+        return;
     };
     NVGLang.prototype.parseTree_node = function (nodes_span, indent, type, ctype) {
         var nodes = { type: type, span: nodes_span, child: [] };
@@ -197,6 +224,142 @@ var NVGLang = (function () {
             span: content_span,
             text: text,
         };
+    };
+    NVGLang.prototype.parseTree_obj = function (nodes_span, indent) {
+        var nodes = { type: "obj", span: nodes_span, child: {} };
+        var i = nodes_span[0];
+        var indentstat = false;
+        var lineinfo = [];
+        while (i < nodes_span[1]) {
+            if (i == 0 || this.code[i - 1] == "\n") {
+                if (indentstat && this.parseTree_checkIndent(i + indent * this.indentSpace)) {
+                    lineinfo.push(["indents", i]);
+                }
+                else {
+                    indentstat = false;
+                    if (this.code.startsWith("&getframe", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["getframe", i]);
+                    }
+                    else if (this.code.startsWith("&length", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["length", i]);
+                    }
+                    else if (this.code.startsWith("&text", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["text", i]);
+                    }
+                    else if (this.code.startsWith("&textcolor", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["textcolor", i]);
+                    }
+                    else if (this.code.startsWith("&color", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["color", i]);
+                    }
+                    else {
+                        lineinfo.push(["invalid", i]);
+                    }
+                }
+            }
+            i++;
+        }
+        lineinfo.push(["eos", i + 1]);
+        console.log("obj", lineinfo);
+        var j = 0;
+        while (j < lineinfo.length - 1) {
+            if (lineinfo[j][0] != "invalid" && lineinfo[j][0] != "indents") {
+                var k = j + 1;
+                console.log(k, lineinfo[j]);
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    var paragraph = { type: "objnode", span: [lineinfo[j][1] + indent * this.indentSpace, lineinfo[j + 1][1] - 1], child: null };
+                    paragraph.child = this.parseTree_func_body([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1);
+                    nodes.child[lineinfo[j][0]] = paragraph;
+                    j = k - 1;
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString());
+                }
+            }
+            j++;
+        }
+        return nodes;
+    };
+    NVGLang.prototype.parseTree_func_body = function (nodes_span, indent) {
+        var nodes = { type: "funcblock", span: nodes_span, child: [] };
+        var i = nodes_span[0];
+        var indentstat = false;
+        var lineinfo = [];
+        while (i < nodes_span[1]) {
+            if (i == 0 || this.code[i - 1] == "\n") {
+                if (indentstat && this.parseTree_checkIndent(i + indent * this.indentSpace)) {
+                    lineinfo.push(["indents", i]);
+                }
+                else {
+                    indentstat = false;
+                    if (this.code.startsWith("if ", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["if", i]);
+                    }
+                    else if (this.code.startsWith("else", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["else", i]);
+                    }
+                    else if (this.code.startsWith("while ", i + indent * this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["while", i]);
+                    }
+                    else {
+                        lineinfo.push(["expr", i]);
+                    }
+                }
+            }
+            i++;
+        }
+        lineinfo.push(["eos", i + 1]);
+        console.log("obj", lineinfo);
+        var j = 0;
+        while (j < lineinfo.length - 1) {
+            if (lineinfo[j][0] == "if") {
+                var cond_expr = { type: "expr", span: [lineinfo[j][1] + indent * this.indentSpace + 3, lineinfo[j + 1][1] - 1], text: "" };
+                cond_expr.text = this.code.slice(cond_expr.span[0], cond_expr.span[1]);
+                var k = j + 1;
+                console.log(k, lineinfo[j]);
+                if (lineinfo[k][0] == "indents") {
+                    while (lineinfo[k][0] == "indents") {
+                        k++;
+                    }
+                    var control = { type: "if", span: [lineinfo[j][1] + indent * this.indentSpace, lineinfo[j + 1][1] - 1], cond: cond_expr, then: null };
+                    control.then = this.parseTree_func_body([lineinfo[j + 1][1], lineinfo[k][1] - 1], indent + 1);
+                    j = k - 1 + 1;
+                    if (lineinfo[j][0] == "else") {
+                        var k_1 = j + 1;
+                        console.log(k_1, lineinfo[j]);
+                        if (lineinfo[k_1][0] == "indents") {
+                            while (lineinfo[k_1][0] == "indents") {
+                                k_1++;
+                            }
+                            control.else = this.parseTree_func_body([lineinfo[j + 1][1], lineinfo[k_1][1] - 1], indent + 1);
+                            j = k_1 - 1;
+                        }
+                    }
+                    nodes.child.push(control);
+                }
+                else {
+                    throw "error ".concat(this.getLineAndCol(i).toString());
+                }
+            }
+            else {
+                var expr = { type: "expr", span: [lineinfo[j][1] + indent * this.indentSpace, lineinfo[j + 1][1] - 1], text: "" };
+                expr.text = this.code.slice(expr.span[0], expr.span[1]);
+                nodes.child.push(expr);
+            }
+            j++;
+        }
+        return nodes;
     };
     return NVGLang;
 }());
--- nvgl.ts
+++ nvgl.ts
@@ -9,16 +9,12 @@ interface NVGL_root {
     func: Array<NVGL_func>,
 }
 type NVGL_node = NVGL_include|NVGL_init|NVGL_timeline|NVGL_obj|NVGL_func;
-type NVGL_func_node = NVGL_line|NVGL_func_if|NVGL_func_while;
 
 type NVGL_nodes = Array<NVGL_node>;
 
 type NVGL_timeline_line = span;
-type NVGL_expr = span;
 type NVGL_obj_functype = span;
 type NVGL_name = span;
-
-type NVGL_contents = NVGL_include_line|NVGL_line;
 interface NVGL_include_line {
     type: "includeline",
     span: span,
@@ -26,6 +22,11 @@ interface NVGL_include_line {
 }
 interface NVGL_line {
     type: "line",
+    span: span,
+    text: string,
+}
+interface NVGL_expr {
+    type: "expr",
     span: span,
     text: string,
 }
@@ -38,7 +39,7 @@ interface NVGL_include {
 interface NVGL_init {
     type: "init",
     span: span,
-    child: Array<NVGL_line>,
+    child: NVGL_block,
 }
 interface NVGL_timeline {
     type: "init",
@@ -48,37 +49,36 @@ interface NVGL_timeline {
 interface NVGL_obj {
     type: "obj",
     span: span,
-    name: NVGL_name,
-    child: NVGL_obj_node,
+    child: Object,
 }
 interface NVGL_obj_node {
     type: "objnode",
     span: span,
-    functype: NVGL_obj_functype,
-    child: NVGL_func_body,
+    name_text: span,
+    child: NVGL_block,
 }
 interface NVGL_func {
     type: "func",
     span: span,
-    child: NVGL_func_body,
+    child: NVGL_block,
 }
-interface NVGL_func_body {
-    type: "funcbody",
+interface NVGL_block {
+    type: "funcblock",
     span: span,
-    child: Array<NVGL_func_node>,
+    child: Array<NVGL_expr|NVGL_if|NVGL_while>,
 }
-interface NVGL_func_if {
+interface NVGL_if {
     type: "if",
     span: span,
     cond: NVGL_expr,
-    then: NVGL_func_body,
-    else: NVGL_func_body,
+    then: NVGL_block,
+    else: NVGL_block,
 }
-interface NVGL_func_while {
+interface NVGL_while {
     type: "while",
     span: span,
     cond: NVGL_expr,
-    child: NVGL_func_body,
+    child: NVGL_block,
 }
 
 class NVGLang {
@@ -150,9 +150,8 @@ class NVGLang {
         console.log(this.parseTree)
         return this.parseTree;
     }
-    parseTree_TLnodes(nodes_span:span,indent:number):NVGL_nodes {
+    parseTree_TLnodes(nodes_span:span,indent:number) {
         this.parseTree ={ type:"root",span:[0,this.code.length],include:null,init:null,timeline:null,obj:[],func:[]};
-        let nodes:NVGL_nodes = [];
         let i:number = nodes_span[0];
         let indentstat:boolean = false; // インデントを受け付けるか
         let lineinfo:Array<["include"|"init"|"timeline"|"obj"|"func"|"indents"|"invalid"|"eos",number]> = [];
@@ -191,7 +190,7 @@ class NVGLang {
             }
             i++;
         }
-        lineinfo.push(["eos",i+1]);
+        lineinfo.push(["eos",i]);
         console.log(lineinfo)
         let j:number = 0;
         while (j<lineinfo.length) {
@@ -210,7 +209,7 @@ class NVGLang {
                 let k:number = j+1;
                 if (lineinfo[k][0]=="indents") {
                     while (lineinfo[k][0]=="indents") {k++;}
-                    this.parseTree.init = this.parseTree_node([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1,"init","line");
+                    this.parseTree.init = this.parseTree_func_body([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1);
                     j = k-1;
                 }
                 else {
@@ -228,9 +227,33 @@ class NVGLang {
                     throw `error ${this.getLineAndCol(i).toString()}`
                 }
             }
+            if (lineinfo[j][0]=="obj") {
+                let k:number = j+1;
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    this.parseTree.obj.push(this.parseTree_obj([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1,"timeline","line"));
+                    j = k-1;
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}`
+                }
+            }
+            if (lineinfo[j][0]=="func") {
+                let k:number = j+1;
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    let func:NVGL_func = {type:"func",span:[lineinfo[j][1]+indent*this.indentSpace,lineinfo[k][1]-1],child:null};
+                    func.child = this.parseTree_func_body([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1);
+                    this.parseTree.func.push(func);
+                    j = k-1;
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}`
+                }
+            }
             j++;
         }
-        return nodes
+        return
     }
     parseTree_node(nodes_span:span,indent:number,type:string,ctype:string):NVGL_node {
         let nodes:NVGL_include = {type:type,span:nodes_span,child:[]};
@@ -264,7 +287,7 @@ class NVGLang {
         }
         return nodes
     }
-    parseTree_content(content_span:span,indent:number,type:string):NVGL_contents { // inline無し暫定版
+    parseTree_content(content_span:span,indent:number,type:string) {
         let text:string = "";
         let i:number = content_span[0];
         while (i<content_span[1]) {
@@ -279,5 +302,135 @@ class NVGLang {
             span: content_span,
             text: text,
         }
+    }
+    parseTree_obj(nodes_span:span,indent:number):NVGL_obj {
+        let nodes:NVGL_obj = {type:"obj",span:nodes_span,child:{}};
+        let i:number = nodes_span[0];
+        let indentstat:boolean = false; // インデントを受け付けるか (nmlnodeとblockの時は受け付ける)
+        let lineinfo:Array<["indents"|"getframe"|"length"|"text"|"textcolor"|"color"|"invalid"|"eos",number]> = [];
+        while (i<nodes_span[1]) {
+            if (i==0||this.code[i-1]=="\n") { // 行頭
+                if (indentstat&&this.parseTree_checkIndent(i+indent*this.indentSpace)) { // インデント有り
+                    lineinfo.push(["indents",i]);
+                }
+                else { // インデント無し
+                    indentstat = false;
+                    if (this.code.startsWith("&getframe",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["getframe",i]);
+                    }
+                    else if (this.code.startsWith("&length",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["length",i]);
+                    }
+                    else if (this.code.startsWith("&text",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["text",i]);
+                    }
+                    else if (this.code.startsWith("&textcolor",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["textcolor",i]);
+                    }
+                    else if (this.code.startsWith("&color",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["color",i]);
+                    }
+                    else {
+                        lineinfo.push(["invalid",i]);
+                    }
+                }
+            }
+            i++;
+        }
+        lineinfo.push(["eos",i+1]);
+        console.log("obj",lineinfo)
+        let j:number = 0;
+        while (j<lineinfo.length-1) {
+            if (lineinfo[j][0]!="invalid"&&lineinfo[j][0]!="indents") {
+                let k:number = j+1;
+                console.log(k,lineinfo[j])
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    let paragraph:NVGL_obj_node = {type:"objnode",span:[lineinfo[j][1]+indent*this.indentSpace,lineinfo[j+1][1]-1],child:null};
+                    paragraph.child = this.parseTree_func_body([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1);
+                    nodes.child[lineinfo[j][0]] = paragraph;
+                    j = k-1;
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}`
+                }
+            }
+            j++;
+        }
+        return nodes
+    }
+    parseTree_func_body(nodes_span:span,indent:number):NVGL_block {
+        let nodes:NVGL_block = {type:"funcblock",span:nodes_span,child:[]};
+        let i:number = nodes_span[0];
+        let indentstat:boolean = false; // インデントを受け付けるか (nmlnodeとblockの時は受け付ける)
+        let lineinfo:Array<["indents"|"if"|"else"|"while"|"expr"|"eos",number]> = [];
+        while (i<nodes_span[1]) {
+            if (i==0||this.code[i-1]=="\n") { // 行頭
+                if (indentstat&&this.parseTree_checkIndent(i+indent*this.indentSpace)) { // インデント有り
+                    lineinfo.push(["indents",i]);
+                }
+                else { // インデント無し
+                    indentstat = false;
+                    if (this.code.startsWith("if ",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["if",i]);
+                    }
+                    else if (this.code.startsWith("else",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["else",i]);
+                    }
+                    else if (this.code.startsWith("while ",i+indent*this.indentSpace)) {
+                        indentstat = true;
+                        lineinfo.push(["while",i]);
+                    }
+                    else {
+                        lineinfo.push(["expr",i]);
+                    }
+                }
+            }
+            i++;
+        }
+        lineinfo.push(["eos",i+1]);
+        console.log("obj",lineinfo)
+        let j:number = 0;
+        while (j<lineinfo.length-1) {
+            if (lineinfo[j][0]=="if") {
+                let cond_expr:NVGL_expr = {type:"expr",span:[lineinfo[j][1]+indent*this.indentSpace+3,lineinfo[j+1][1]-1],text:""};
+                cond_expr.text = this.code.slice(cond_expr.span[0],cond_expr.span[1]);
+                let k:number = j+1;
+                console.log(k,lineinfo[j])
+                if (lineinfo[k][0]=="indents") {
+                    while (lineinfo[k][0]=="indents") {k++;}
+                    let control:NVGL_if = {type:"if",span:[lineinfo[j][1]+indent*this.indentSpace,lineinfo[j+1][1]-1],cond:cond_expr,then:null};
+                    control.then = this.parseTree_func_body([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1);
+                    j = k-1 +1;
+                    if (lineinfo[j][0]=="else") {
+                        let k:number = j+1;
+                        console.log(k,lineinfo[j])
+                        if (lineinfo[k][0]=="indents") {
+                            while (lineinfo[k][0]=="indents") {k++;}
+                            control.else = this.parseTree_func_body([lineinfo[j+1][1],lineinfo[k][1]-1],indent+1);
+                            j = k-1;
+                        }
+                    }
+                    nodes.child.push(control);
+                }
+                else {
+                    throw `error ${this.getLineAndCol(i).toString()}`
+                }
+            }
+            else {
+                let expr:NVGL_expr = {type:"expr",span:[lineinfo[j][1]+indent*this.indentSpace,lineinfo[j+1][1]-1],text:""};
+                expr.text = this.code.slice(expr.span[0],expr.span[1]);
+                nodes.child.push(expr);
+            }
+            j++;
+        }
+        return nodes
     }
 }
