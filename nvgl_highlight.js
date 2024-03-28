9b6b0380e725abdf300dcc709027a5bb68264463
bem130 <haruki070617@gmail.com>
2024-03-29 02:25:59 +0900

    improve the syntax highlight a little

--- nvgl_highlight.js
+++ nvgl_highlight.js
@@ -18,12 +18,17 @@ const tokenizer = {
             [/[^(*\n]+/, 'comment'],
             [/[(\*]/, 'comment']
         ],
+        MLstring: [
+            [/.*/, 'string', '@pop'],
+        ],
         tlobj_a: [
-            [/@(?=((includes|init|timeline) *{))/,{token:"tlobj.symbol",next:"@tlobja_"}],
+            [/@(?=((includes|init|timeline) *{?))/,{token:"tlobj.symbol",next:"@tlobja_"}],
             [/\{/, { token: 'delimiter.brace', next: '@block' }]
         ],
         tlobj_b: [
-            [/@(?=((item|obj) +([0-9a-zA-Z]|_)+ *{))/,{token:"tlobj.symbol",next:"@tlobjb_"}],
+            [/@(?=((item|obj)?))/,{token:"tlobj.symbol",next:"@tlobjb_"}],
+            [/@(?=((item|obj) *([0-9a-zA-Z]|_)+?))/,{token:"tlobj.symbol",next:"@tlobjb_"}],
+            [/@(?=((item|obj) +([0-9a-zA-Z]|_)+? *{?))/,{token:"tlobj.symbol",next:"@tlobjb_"}],
             [/\{/, { token: 'delimiter.brace', next: '@block' }]
         ],
         tlobja_: [
@@ -33,10 +38,20 @@ const tokenizer = {
             [/(item|obj)/,"tlobj.type"],
             [/([0-9a-zA-Z]|_)+/,"tlobj.name","@pop"],
         ],
+        sexpr: [
+            {include: "@block"},
+            [/\)/,{token:"delimiter.parentheses.sexpr", next: "@pop"}],
+        ],
+        exprgroup: [
+            {include: "@block"},
+            [/\)/,{token:"delimiter.parentheses.exprgroup", next: "@pop"}],
+        ],
         block: [
             [/{/,{token:"delimiter.brace", next: "@block"}],
             [/}/,{token:"delimiter.brace", next: "@pop"}],
-
+            
+            [/\\\(/,{token:"delimiter.parentheses.sexpr", next: "@sexpr"}],
+            [/\(/,{token:"delimiter.parentheses.exprgroup", next: "@exprgroup"}],
             [/#[0-9a-fA-F]{8}/,"color"],
             [/#[0-9a-fA-F]{6}/,"color"],
             [/#[0-9a-fA-F]{4}/,"color"],
@@ -44,8 +59,13 @@ const tokenizer = {
             [/#\*/, { token: 'comment.enclosure', next: '@Bcomment' }],
             [/#/, { token: 'comment.enclosure', next: '@Lcomment' }],
             [/"(\\.|[^"\\])*"/, "string"],
-            [/^ *:.*/, "string"],
+            [/^ *:/, { token: 'MLstringstart', next: '@MLstring' }],
+            [/([0-9a-zA-Z]|_)+ *(?=<:)/, "variable.loc"],
+            [/([0-9a-zA-Z]|_)+ *(?=<::)/, "variable.loc"],
             [/!(return|break|continue) */, "keyword.control"],
+            [/(if|else|elif|while|times) */, "keyword.control"],
+            [/[0-9]+/, "number"],
+            [/([0-9a-zA-Z]|_)+(?=:)/, "objkey"],
             [/([0-9a-zA-Z]|_)+/, "variable"],
         ],
         // MLTextA: [
@@ -62,12 +82,18 @@ const theme = {
     rules: [
         { token: "comment", fontStyle: "italic"},
         { token: "comment.enclosure", foreground: "#607060"},
-        { token: "delimiter.brace", foreground: "#805bff"},
+        { token: "delimiter.brace", foreground: "#b0abff"},
         { token: "tlobj.symbol", foreground: "#bbbbff"},
-        { token: "tlobj.type", foreground: "#47a2df"},
-        { token: "tlobj.name", foreground: "#ffcc75"},
-        { token: "color", foreground: "#99bf9f"},
+        { token: "tlobj.type", foreground: "#f772df"},
+        { token: "tlobj.name", foreground: "#ffcc85"},
+        { token: "color", foreground: "#79bf9f"},
         { token: "keyword.control", foreground: "#ff5999"},
+        { token: "objkey", foreground: "#6786c2"},
+        { token: "delimiter.parentheses.sexpr", foreground: "#b0abbf"},
+        { token: "delimiter.parentheses.exprgroup", foreground: "#b0abaf"},
+        { token: "MLstringstart", foreground: "#707b2f"},
+        { token: "variable", foreground: "#47a2df"},
+        { token: "variable.loc", foreground: "#ffcc85"},
     ],
     colors: {},
 }
